32357eff569765259a9049bc853606e1697600d9
Harry Brundage <harry.brundage@gmail.com>
2020-04-16 17:00:44 -0400

    Flesh out contact form in UI framework

---
+++ components/layout/TrackingScripts.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+export const TrackingScripts =
+  process.env.NODE_ENV == "production" ? (
+    <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161950128-1"></script>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "UA-161950128-1", { anonymize_ip: false });
+`,
+        }}
+      />
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+  !(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = "2.0";
+    n.agent = "plwebflow";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+  fbq("init", "268176577514147");
+  fbq("track", "PageView");`,
+        }}
+      />
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+      (window.heap = window.heap || []),
+        (heap.load = function (e, t) {
+          (window.heap.appid = e), (window.heap.config = t = t || {});
+          var r = document.createElement("script");
+          (r.type = "text/javascript"), (r.async = !0), (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
+          var a = document.getElementsByTagName("script")[0];
+          a.parentNode.insertBefore(r, a);
+          for (
+            var n = function (e) {
+                return function () {
+                  heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              },
+              p = [
+                "addEventProperties",
+                "addUserProperties",
+                "clearEventProperties",
+                "identify",
+                "resetIdentity",
+                "removeEventProperty",
+                "setEventProperties",
+                "track",
+                "unsetEventProperty",
+              ],
+              o = 0;
+            o < p.length;
+            o++
+          )
+            heap[p[o]] = n(p[o]);
+        });
+      if (window.location.host.indexOf("together-apart.ca") != -1) {
+        heap.load("415434005");
+      } else {
+        heap.load("3041848733");
+      }
+      `,
+        }}
+      />
+    </>
+  ) : null;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,75 +1,98 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { FormControl } from "baseui/form-control";
+import { Input } from "baseui/input";
+import { Textarea } from "baseui/textarea";
 import { Layout } from "../components/layout/Layout";
 import { Meta } from "../components/Meta";
 import { StaticLink } from "../components/StaticLink";
+import { Button } from "baseui/button";
+import { useStyletron } from "baseui";
+import { ParagraphMedium } from "baseui/typography";
 
-export default (_props: {}) => (
-  <Layout>
-    <Meta title="Contact Us" />
-    <div className="narrow-container">
-      <div className="w-form">
-        <form id="wf-form-Contact-Form" name="wf-form-Contact-Form" data-name="Contact Form">
-          <h2>Contact Us</h2>
-          <p>
-            <strong>Note:</strong> If you&#x27;d like to submit a business for inclusion on Together Apart, that&#x27;s done{" "}
-            <StaticLink href="/submit-a-business">over here</StaticLink>.
-          </p>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="input w-input"
-            maxLength={256}
-            name="name"
-            data-name="Name"
-            placeholder="Enter your name"
-            id="name"
-            required
-          />
-          <label htmlFor="Email">Email</label>
-          <input
-            type="email"
-            className="input w-input"
-            maxLength={256}
-            name="Email"
-            data-name="Email"
-            placeholder="youremail@yourdomain.com"
-            id="Email"
-            required
-          />
-          <label htmlFor="Website">Message</label>
-          <div className="w-embed">
-            <textarea className="w-input input" name="message" placeholder="Enter your message here..." required></textarea>
-          </div>
-          <input
-            type="submit"
-            value="Submit"
-            data-wait="Please wait..."
-            data-w-id="022b6b15-2675-7934-f9f7-ba9c53ebfd1f"
-            className="btn w-button"
-          />
-        </form>
-        <div className="success w-form-done">
-          <div className="success-wrapper">
-            <div
-              data-w-id="7d6eafb1-a27f-0300-96c1-ce5501eb7082"
-              data-animation-type="lottie"
-              data-src="https://uploads-ssl.webflow.com/5e7a31dcdd44a76199b8112d/5e7a31dd4dd994c4414fbd0d_lf30_editor_FcQPfq.json"
-              data-loop="0"
-              data-direction="1"
-              data-autoplay="0"
-              data-is-ix2-target="1"
-              data-renderer="svg"
-              data-default-duration="1.5015014403440954"
-              data-duration="0"
-              className="lottie-animation"
-            ></div>
-            <div>Thank you! We&#x27;ll try to get back to you within 24 hours.</div>
-          </div>
-        </div>
-        <div className="error-message w-form-fail">
-          <div>Oops! Something went wrong while submitting the form.</div>
+export const ContactForm = (_props: {}) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().max(255, "Must be 255 characters or less").required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      message: Yup.string().required("Required"),
+    }),
+    onSubmit: async (values, helpers) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        helpers.setSubmitting(false);
+      }, 1000);
+    },
+  });
+
+  return (
+    <div className="w-form">
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Contact Us</h2>
+        <p>
+          <strong>Note:</strong> If you&#x27;d like to submit a business for inclusion on Together Apart, that&#x27;s done{" "}
+          <StaticLink href="/submit-a-business">over here</StaticLink>.
+        </p>
+        <FormControl label="Name" error={formik.errors.name}>
+          <Input id="name" {...formik.getFieldProps("name")} placeholder="Enter your name ..." error={!!formik.errors.name} />
+        </FormControl>
+
+        <FormControl label="Email" error={formik.errors.email}>
+          <Input id="email" {...formik.getFieldProps("email")} placeholder="hello@example.com" error={!!formik.errors.email} />
+        </FormControl>
+
+        <FormControl label="Message" error={formik.errors.message}>
+          <Textarea id="message" {...formik.getFieldProps("message")} error={!!formik.errors.message} />
+        </FormControl>
+
+        <Button type="submit" disabled={formik.isSubmitting} onClick={() => formik.handleSubmit()}>
+          {formik.isSubmitting ? "Submitting ..." : "Submit"}
+        </Button>
+      </form>
+      <div className="success w-form-done">
+        <div className="success-wrapper">
+          <div
+            data-w-id="7d6eafb1-a27f-0300-96c1-ce5501eb7082"
+            data-animation-type="lottie"
+            data-src="https://uploads-ssl.webflow.com/5e7a31dcdd44a76199b8112d/5e7a31dd4dd994c4414fbd0d_lf30_editor_FcQPfq.json"
+            data-loop="0"
+            data-direction="1"
+            data-autoplay="0"
+            data-is-ix2-target="1"
+            data-renderer="svg"
+            data-default-duration="1.5015014403440954"
+            data-duration="0"
+            className="lottie-animation"
+          ></div>
+          <div>Thank you! We&#x27;ll try to get back to you within 24 hours.</div>
         </div>
       </div>
+      <div className="error-message w-form-fail">
+        <div>Oops! Something went wrong while submitting the form.</div>
+      </div>
     </div>
-  </Layout>
-);
+  );
+};
+export default (_props: {}) => {
+  const [css] = useStyletron();
+
+  return (
+    <Layout>
+      <Meta title="Contact Us" />
+      <div className="narrow-container">
+        <ParagraphMedium $style={{ textAlign: "center" }}>
+          If you - as a patron or as a business owner - see a mistake, want to revise something for accuracy&apos;s sake, or anything else,
+          please drop us a quick note below. We&apos;ll get back to you as soon as possible. You can email us at{" "}
+          <a href="mailto:hi@together-apart.ca">hi@together-apart.ca</a>, or use the form below to get in touch.
+        </ParagraphMedium>
+        <ContactForm />
+      </div>
+    </Layout>
+  );
+};
