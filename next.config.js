0ef57f2416c096d83e3b5199131cd78245ed09a3
Harry Brundage <harry.brundage@gmail.com>
2020-04-24 13:02:27 -0400

    Add preconnect and analyze

--- next.config.js
+++ next.config.js
@@ -3,34 +3,38 @@ const webpack = require("webpack");
 const withImages = require("next-images");
 const withTM = require("next-transpile-modules")(["lodash-es", "react-blurhash"]);
 const nextSourceMaps = require("@zeit/next-source-maps")({ devtool: "cheap-module-source-map" });
-
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 if (!process.env.CURRENT_SITE) process.env.CURRENT_SITE = "ottawa";
 
-module.exports = withImages(
-  nextSourceMaps(
-    withTM({
-      env: {
-        SENTRY_DSN: process.env.SENTRY_DSN,
-        ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-        CURRENT_SITE: process.env.CURRENT_SITE,
-      },
-      webpack: (config, { isServer, buildId }) => {
-        config.plugins.push(
-          new webpack.DefinePlugin({
-            "process.env.SENTRY_RELEASE": JSON.stringify(buildId),
-          })
-        );
+module.exports = withBundleAnalyzer(
+  withImages(
+    nextSourceMaps(
+      withTM({
+        env: {
+          SENTRY_DSN: process.env.SENTRY_DSN,
+          ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
+          CURRENT_SITE: process.env.CURRENT_SITE,
+        },
+        webpack: (config, { isServer, buildId }) => {
+          config.plugins.push(
+            new webpack.DefinePlugin({
+              "process.env.SENTRY_RELEASE": JSON.stringify(buildId),
+            })
+          );
 
-        if (!isServer) {
-          config.resolve.alias["@sentry/node"] = "@sentry/browser";
-        }
+          if (!isServer) {
+            config.resolve.alias["@sentry/node"] = "@sentry/browser";
+          }
 
-        config.externals = config.externals || {};
-        config.externals["styletron-server"] = "styletron-server";
+          config.externals = config.externals || {};
+          config.externals["styletron-server"] = "styletron-server";
 
-        return config;
-      },
-    })
+          return config;
+        },
+      })
+    )
   )
 );
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,23 +28,18 @@ export default class StyledDocument extends Document<ExtraProps> {
     return (
       <Html lang="en">
         <Head>
+          {[
+            "https://fonts.googleapis.com",
+            "https://connect.facebook.net",
+            "https://www.google-analytics.com",
+            "https://cdn.heapanalytics.com",
+            "https://global-uploads.webflow.com",
+          ].map((url) => (
+            <link key={url} rel="preconnect" href={url} />
+          ))}
           <noscript dangerouslySetInnerHTML={{ __html: `<!--\n${Greeting}-->` }} />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `WebFont.load({google:{families:["DM Sans:regular,500,700","DM Serif Display:regular"]}});`,
-            }}
-          />
-          <meta
-            content="https://uploads-ssl.webflow.com/5e7a31dcdd44a76199b8112d/5e7e1bb3a3d3d67f4cef9bed_together-apart-og.png"
-            property="og:image"
-          />
-          <meta content="summary" name="twitter:card" />
-          <link href="/images/favicon.png" rel="shortcut icon" type="image/x-icon" />
-          <link href="/images/webclip.png" rel="apple-touch-icon" />
-          {TrackingScripts}
+          <link href={"https://fonts.googleapis.com/css?family=DM+Sans:400,500,700|DM+Serif+Display&display=swap"} rel="stylesheet" />
           {this.props.stylesheets.map((sheet, i) => (
             <style
               className="_styletron_hydrate_"
@@ -54,6 +49,11 @@ export default class StyledDocument extends Document<ExtraProps> {
               key={i}
             />
           ))}
+          <meta content="summary" name="twitter:card" />
+          <meta content="/images/together-apart-og.png" property="og:image" />
+          <link href="/images/favicon.png" rel="shortcut icon" type="image/x-icon" />
+          <link href="/images/webclip.png" rel="apple-touch-icon" />
+          {TrackingScripts}
         </Head>
         <body>
           <Main />
