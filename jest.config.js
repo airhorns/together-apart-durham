687efcf7df16cf88594fc4056029f5eff9e0debb
Harry Brundage <harry.brundage@gmail.com>
2020-04-27 11:51:05 -0400

    Fix one part of the tests errors with requiring images

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es|react-blurhash)", "^.+\\.module\\.(css|sass|scss)$"],
   moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js",
   },
 };
