3406990b4bd20000608f2efcbba44f6b28e3d974
Harry Brundage <harry.brundage@gmail.com>
2020-04-29 14:36:05 -0400

    Clean up search query url states

--- src/components/search/searchClient.ts
+++ src/components/search/searchClient.ts
@@ -2,6 +2,7 @@ import algoliasearch from "algoliasearch/lite";
 import querystring from "qs"; // use qs and not query-string cause it supports nested object serialization which algolia search states require
 import { assert } from "../../lib/utils";
 import { CurrentSite } from "../../lib/sites";
+import { omit } from "lodash-es";
 
 export const searchClient = algoliasearch(assert(process.env.ALGOLIA_APP_ID), assert(CurrentSite.algoliaAPIKey));
 
@@ -15,5 +16,19 @@ export const paramsToSearchState = (params: any): SearchState => params;
 export const pathToSearchState = (path: string): SearchState =>
   path.includes("?") ? paramsToSearchState(querystring.parse(path.substring(path.indexOf("?") + 1))) : {};
 
-export const searchStateToURL = (searchState: SearchState) =>
-  searchState ? `${window.location.pathname}?${querystring.stringify(searchState)}` : "";
+export const searchStateToURL = (searchState: SearchState) => {
+  const segments = [window.location.pathname];
+  if (searchState) {
+    const params = omit(searchState, "configure");
+    if (params.page == 1) {
+      delete params.page;
+    }
+    if (params.query == "") {
+      delete params.query;
+    }
+    if (Object.keys(params).length > 0) {
+      segments.push("?" + querystring.stringify(params));
+    }
+  }
+  return segments.join("");
+};
