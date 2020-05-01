ef31e645d2a11a874f81305cc90ff37d62b788a0
Harry Brundage <harry.brundage@gmail.com>
2020-05-01 16:22:57 -0400

    Pollyjs for recorded test interactions?

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,6 +1,19 @@
-// optional: configure or set up a testing framework before each test
-// if you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// used for __tests__/testing-library.js
-// learn more: https://github.com/testing-library/jest-dom
+import path from "path";
 import "@testing-library/jest-dom/extend-expect";
+import { Polly } from "@pollyjs/core";
+import NodeHttpAdapter from "@pollyjs/adapter-node-http";
+import FSPersister from "@pollyjs/persister-fs";
+import { setupPolly } from "setup-polly-jest";
+
+Polly.register(NodeHttpAdapter);
+Polly.register(FSPersister);
+
+setupPolly({
+  adapters: ["node-http"],
+  persister: "fs",
+  persisterOptions: {
+    fs: {
+      recordingsDir: path.resolve(__dirname, "./__recordings__"),
+    },
+  },
+});
